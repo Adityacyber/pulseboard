057c9b504b82e0a14ad87c3c77f5ce0413036382
Adityacyber <aoaditya1996@gmail.com>
2026-05-14 23:40:53 +0530

    URI update

--- server/src/socket.js
+++ server/src/socket.js
@@ -5,8 +5,8 @@ let io;
 export const initSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
-      methods: ["GET", "POST"],
+      origin: process.env.CLIENT_URL,
+      methods: ["GET", "POST", "PUT", "DELETE"],
     },
   });
 
